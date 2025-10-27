1227587802f6a23c4760db4397fd5bc90b8b37cb
Janki Marandi <80061704+mjanki1054@users.noreply.github.com>
2025-10-27 14:53:31 +0530

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -8,7 +8,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
-  <?php include '../common/header.php'; ?>
+  <?php include 'common/header.php'; ?>
   <!-- style -->
   <link rel="stylesheet" href="/CSS/style.css" />
 </head>
