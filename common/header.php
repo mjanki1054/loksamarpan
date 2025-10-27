25996264831d13f6ff0c555a9b9b2130edcf80ef
Janki Marandi <80061704+mjanki1054@users.noreply.github.com>
2025-10-27 15:07:29 +0530

    Update header.php

--- common/header.php
+++ common/header.php
@@ -1,4 +1,5 @@
 <?php
+// 🛡 Security Headers (must come before any HTML output)
 header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: SAMEORIGIN");
 header("Referrer-Policy: no-referrer-when-downgrade");
@@ -7,8 +8,8 @@ header("Access-Control-Allow-Origin: *");
 
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
   <div class="container">
-    <a class="navbar-brand d-flex align-items-center" href="#">
-      <img src="Asset/loksam.png" alt="LokSamarpan Logo" />
+    <a class="navbar-brand d-flex align-items-center" href="/loksamarpan/index.php">
+      <img src="/loksamarpan/Asset/loksam.png" alt="LokSamarpan Logo" />
       <span class="ms-2">LOKSAMARPAN TRUST</span>
     </a>
 
@@ -26,10 +27,11 @@ header("Access-Control-Allow-Origin: *");
 
     <div class="collapse navbar-collapse" id="navmenu">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="project.php" class="nav-link">Projects</a></li>
-        <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+        <li class="nav-item"><a href="/loksamarpan/index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="/loksamarpan/about.php" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="/loksamarpan/project.php" class="nav-link">Projects</a></li>
+        <li class="nav-item"><a href="/loksamarpan/contact.php" class="nav-link">Contact</a></li>
+
         <li class="nav-item dropdown">
           <a
             class="nav-link dropdown-toggle"
@@ -42,13 +44,14 @@ header("Access-Control-Allow-Origin: *");
             More
           </a>
           <ul class="dropdown-menu" aria-labelledby="moreDropdown">
-            <li><a class="dropdown-item" href="ourteam.php">Our Team</a></li>
-            <li><a class="dropdown-item" href="gallery.php">Gallery</a></li>
-            <li><a class="dropdown-item" href="news.php">News & Events</a></li>
+            <li><a class="dropdown-item" href="/loksamarpan/ourteam.php">Our Team</a></li>
+            <li><a class="dropdown-item" href="/loksamarpan/gallery.php">Gallery</a></li>
+            <li><a class="dropdown-item" href="/loksamarpan/news.php">News & Events</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="career.php">Careers</a></li>
+            <li><a class="dropdown-item" href="/loksamarpan/career.php">Careers</a></li>
           </ul>
         </li>
+
         <li class="nav-item">
           <a href="#" class="btn btn-donate ms-3">Donate Now</a>
         </li>
